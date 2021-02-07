1198b5fb5b913160acd490b8c5e8a3690a3d7eed
Anderson <anderaddiction@gmail.com>
2021-02-07 18:56:38 -0400

    1- Se trabajó el un codigo jquery, ajax para actualizar la foto de perfil del empleado. Esto servidra para los usuarios y propietarios. El error que tengo hasta ahora es que el formulario de datos se envia vacio. El código jquery se implemento en la vista edit.blade.php porque funciona desde alli una vez funcione debo trabajrlos en el archivo agais.js 2- Se borro el paquete editor.zip porque no se neceita aún

--- resources/views/auth/employees/edit.blade.php
+++ resources/views/auth/employees/edit.blade.php
@@ -2,18 +2,49 @@
     @method('PUT')
     @section('avatar')
     	<label for="wage">@lang('Photo')</label>
-    	<form method="POST" action="{{ route('employees.update', $employee) }}" rol="form" id="formElem" enctype="multipart/form-data">
     		@method('PUT')
 			<div class="input-group mb-3">
-				<div class="input-group-prepend">
-					<input class="btn btn-primary" type="submit" id="save-image" name="save-image" value="{{ __('Upload') }}">
-				</div>
-			  	<div class="custom-file">
-				  	<input type="file" class="custom-file-input" id="avatar" name="avatar" value="{{ old('note', $employee->avatar ) }}">
-				  	<label class="custom-file-label" for="avatar">@lang('Choose file')</label>
-				</div>
-			</form>
+			<!--<div class="input-group-prepend">
+				<input class="btn btn-primary" type="button" id="save-image" name="save-image" value="{{ __('Upload') }}">-->
+				<input type="file" id="avatar" name="avatar" value="{{ old('note', $employee->avatar ) }}">
+			<!--</div>
+		  	<div class="custom-file">
+			  	<input type="file" class="custom-file-input" id="avatar" name="avatar" value="{{ old('note', $employee->avatar ) }}">
+			  	<label class="custom-file-label" for="avatar">@lang('Choose file')</label>
+			</div>-->
 		</div>
     @endsection
     @include('auth.employees._form')
 </form>
+<script type="text/javascript">
+	$('.btn-update').click(function(event) {
+        /* Act on the event */
+        event.preventDefault();
+        var token       = $('meta[name="csrf-token"]').attr('content');
+        var form = $('#form')[0];
+        var data = new FormData(form);
+        var route = $("#form").attr('action');
+        console.log(route);
+        console.log(data);
+
+        $.ajax({
+        	url: route,
+        	type: 'POST',
+        	data: data,
+        	headers: {'X-CSRF-Token': token},
+        	contentType: false,
+        	cache: false,
+        	processData: false,
+        })
+        .done(function(data) {
+        	console.log(data);
+        })
+        .fail(function() {
+        	console.log("error");
+        })
+        .always(function() {
+        	console.log("complete");
+        });
+        
+    });
+</script>
